f5a385fd273cfd310af39f23fdf2d661bc7f6642
Proryon <85037053+Proryon@users.noreply.github.com>
2021-10-31 22:03:29 +0100

    Update and rename README.md to index.js

--- README.md
+++
@@ -1,1 +0,0 @@
-# proyron.github.io
---
+++ index.js
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+
+<head><link rel="icon" type="image/png" sizes="16x16" href="phdp.png"><title>UniBot</title></head>
+
+<h1>Vous êtes perdu...</h1>
+
+<body></body>
+</html>
